27f79e89693b2b4dcc46bf4ecede013aceec680d
brunop6 <peresbruno6@gmail.com>
2023-08-27 17:25:41 -0300

    Vinculação de novos participantes - Otimização das consultas das frequências por aula; - Vinculação dos participantes não adicionados em uma turma; - Ajustada a vizualização das frequências ao trocar de turma; - Cadastrados os participantes confirmados da tarde.

--- src/app/components/frequency/frequency.component.ts
+++ src/app/components/frequency/frequency.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateLessonDlgComponent } from './dialogs/create-lesson-dlg/create-lesson-dlg.component';
-import { collection, doc, getDocs, getFirestore, orderBy, query, where, updateDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, getFirestore, orderBy, query, where, updateDoc, addDoc, getDoc } from 'firebase/firestore';
 import { StudentService } from 'src/app/shared/services/student.service';
 import { DocumentData } from '@angular/fire/compat/firestore';
 import * as DateFormat from 'src/app/shared/functions/dateFormat'
@@ -15,8 +15,10 @@ export class FrequencyComponent implements OnInit {
   private db = getFirestore();
   private changedStudents: number[] = [];
   protected lessons: DocumentData[] = [];
-  protected selectedLesson:DocumentData;
+  protected selectedLesson: DocumentData;
   protected students: DocumentData[] = [];
+  protected studentsClass: DocumentData[] = [];
+  protected nonLinkedStudents: DocumentData[] = [];
   protected df: any = DateFormat;
 
   constructor(
@@ -24,13 +26,20 @@ export class FrequencyComponent implements OnInit {
     private studentService: StudentService
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.studentsClass = await this.studentService.getStudentsByClass('Manhã');
     this.getLessons().then(() => {
-      this.getStudentsFrequency(this.selectedLesson);
+      this.getStudentsFrequency(this.selectedLesson.id);
     });
   }
 
-  async getLessons(){
+  @HostListener("window:beforeunload", ["$event"]) unloadHandler(event: Event) {
+    this.lessons.forEach(lesson => {
+      sessionStorage.removeItem(lesson.id)
+    })
+  }
+
+  async getLessons() {
     this.lessons = [];
 
     const studentsClass = document.querySelector('#class') as HTMLSelectElement;
@@ -50,31 +59,42 @@ export class FrequencyComponent implements OnInit {
         });
       })
 
-      this.selectedLesson = this.lessons[this.lessons.length-1];
+      this.selectedLesson = this.lessons[this.lessons.length - 1];
     })
   }
 
-  async getStudentsFrequency(lesson: DocumentData) {
+  async getStudentsFrequency(lessonId: string) {
     this.students = [];
     this.changedStudents = [];
 
-    if(lesson === undefined) return;
-    
-    const q = query(
-      collection(this.db, 'frequencies'),
-      where('lessonId', '==', lesson.id),
-      orderBy('studentName')
-    );
+    if (lessonId === undefined) return;
 
-    await getDocs(q).then((querySnapshot) => {
-      querySnapshot.forEach(async (doc) => {
-        this.students.push({
-          freqId: doc.id,
-          name: doc.data()['studentName'],
-          attendance: doc.data()['attendance']
+    if (sessionStorage.getItem(lessonId) == null) {
+      const q = query(
+        collection(this.db, 'frequencies'),
+        where('lessonId', '==', lessonId),
+        orderBy('studentName')
+      );
+
+      await getDocs(q).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.students.push({
+            freqId: doc.id,
+            attendance: doc.data()['attendance'],
+            studentId: doc.data()['studentId'],
+            name: doc.data()['studentName']
+          })
         })
+
+        sessionStorage.setItem(lessonId, JSON.stringify(this.students))
       })
-    })
+    } else {
+      this.students = JSON.parse(sessionStorage.getItem(lessonId));
+    }
+
+    this.nonLinkedStudents = this.studentsClass.filter(student =>
+      this.students.find(linkedStudent => linkedStudent.studentId == student.id) === undefined
+    );
   }
 
   openDialogCreateLesson(): void {
@@ -87,30 +107,56 @@ export class FrequencyComponent implements OnInit {
     });
   }
 
-  changeClass() {
+  async changeClass() {
+    const studentsClass = document.querySelector('#class') as HTMLSelectElement;
+
+    this.studentsClass = await this.studentService.getStudentsByClass(studentsClass.value);
     this.getLessons().then(() => {
-      this.getStudentsFrequency(this.selectedLesson);
+      this.getStudentsFrequency(this.selectedLesson.id);
     });
   }
 
   changeDate(lessonIndex: number) {
     this.selectedLesson = this.lessons[lessonIndex];
 
-    this.getStudentsFrequency(this.selectedLesson);
+    this.getStudentsFrequency(this.selectedLesson.id);
   }
 
   changeAttendance(studentIndex: number) {
     this.students[studentIndex].attendance = !this.students[studentIndex].attendance;
-
+    
     //Salvando os estudantes alterados p/ alterar no banco apenas os necessários
-    if(this.changedStudents.find((index) => index == studentIndex) === undefined){
+    if (this.changedStudents.find((index) => index == studentIndex) === undefined) {
       this.changedStudents.push(studentIndex);
     } else {
       this.changedStudents.splice(studentIndex, 1);
     }
+    
+    sessionStorage.setItem(this.selectedLesson.id, JSON.stringify(this.students));
   }
 
-  saveFrequency(){
+  async linkStudent() {
+    const select = document.querySelector("#addStudent") as HTMLSelectElement;
+
+    await addDoc(collection(this.db, 'frequencies'), {
+      lessonId: this.selectedLesson.id,
+      studentId: select.options[select.selectedIndex].value,
+      studentName: select.options[select.selectedIndex].text,
+      attendance: true
+    }).then((newDoc) => {
+      this.students.push({
+        freqId: newDoc.id,
+        attendance: true,
+        studentId: select.options[select.selectedIndex].value,
+        name: select.options[select.selectedIndex].text
+      })
+      this.nonLinkedStudents = this.nonLinkedStudents.filter(item => item.id != select.options[select.selectedIndex].value);
+      
+      select.options.remove(select.selectedIndex);
+    })
+  }
+
+  saveFrequency() {
     this.changedStudents.forEach((index) => {
       updateDoc(doc(this.db, 'frequencies', this.students[index].freqId), {
         attendance: this.students[index].attendance
